20799e197dfa52feafe3076313a83b7f1106457b
chintitomasud3 <mrahman021992@gmail.com>
2023-03-23 15:56:45 +0600

    pages

---
+++ haire.js
@@ -0,0 +1,13 @@
+function bigfont(){
+document.getElementById("fontboro").style.fontSize = "90px";
+document.getElementById("bigfont").style.display = "none";
+document.getElementById("insert").innerHTML="<h1> bolod masudur rahman</h1>"
+}
+
+var masud= "hello"+"haire";
+console.log(masud);
+
+
+function styleme(){
+    alert("kaj hoache");
+}
